a27cdf65eaf9c1ca022abaebc2613da21dbaf57f
Weslley Barboza <weslleybarboza@gmail.com>
2024-11-12 22:16:42 +0100

    dremio set up

---
+++ dags/dremio_add_sources.py
@@ -0,0 +1,103 @@
+import requests
+import json
+
+DREMIO_API_URL = 'http://localhost:9047/'
+DREMIO_USERNAME = 'dremio'
+DREMIO_PASSWORD = 'dremio123'
+
+# List of data sources to add on Dremio
+sources = [
+    {
+        "name": "lakehouse",
+        "type": "NESSIE",
+        "config": {
+            "nessieEndpoint": "http://nessie:19120/api/v2",
+            "nessieAuthType": "NONE",
+            "asyncEnabled": True,
+            "isCachingEnabled": True,
+            "maxCacheSpacePct": 100,
+            "credentialType": "ACCESS_KEY",
+            "awsAccessKey": "minio",
+            "awsAccessSecret": "minio123",
+            "awsRootPath": "lakehouse",
+            "propertyList": [
+            {
+                "name": "fs.s3a.path.style.access",
+                "value": "true"
+            },
+            {
+                "name": "fs.s3a.endpoint",
+                "value": "http://minio:9000"
+            },
+            {
+                "name": "dremio.s3.compat",
+                "value": "true"
+            }
+            ],
+            "secure": False,
+            "isCachingEnabled": True,
+            "maxCacheSpacePct": 100
+        }
+    },
+    {        
+        "name": "landing-zone",
+        "type": "S3",
+        "config": {
+            "accessKey": "minio",
+            "accessSecret": "minio123",
+            "secure": False,
+            "propertyList": [
+            {
+                "name": "fs.s3a.path.style.access",
+                "value": "true"
+            },
+            {
+                "name": "fs.s3a.endpoint",
+                "value": "minio:9000"
+            },
+            ],
+            "rootPath": "/",
+            "enableAsync": False,
+            "compatibilityMode": True,
+            "isCachingEnabled": True,
+            "maxCacheSpacePct": 100,
+            "whitelistedBuckets": [
+            "landing-zone"
+            ],
+            "requesterPays": False,
+            "enableFileStatusCheck": True,
+            "defaultCtasFormat": "ICEBERG",
+            "isPartitionInferenceEnabled": False,
+            "credentialType": "ACCESS_KEY"
+        }
+    }
+]
+
+# Authenticate and get token
+def authenticate():
+    auth_url = f"{DREMIO_API_URL}/apiv2/login"
+    response = requests.post(auth_url, json={"userName": DREMIO_USERNAME, "password": DREMIO_PASSWORD})
+    if response.status_code == 200:
+        token = response.json()['token']
+        return token
+    else:
+        raise Exception(f"Failed to authenticate: {response.text}")
+
+# Add source using Dremio API
+def add_source(token, source):
+    headers = {
+        'Authorization': f'_dremio {token}',  # Space between _dremio and token
+        'Content-Type': 'application/json'
+    }
+    url = f"{DREMIO_API_URL}/api/v3/source"
+    response = requests.post(url, headers=headers, data=json.dumps(source))
+    if response.status_code == 200 or response.status_code == 201:
+        print(f"Successfully added source {source['name']}")
+    else:
+        print(f"Failed to add source {source['name']}: {response.text}")
+
+# Main
+if __name__ == "__main__":
+    token = authenticate()
+    for source in sources:  # Iterate over the list of sources
+        add_source(token, source)
--- readme.md
+++ readme.md
@@ -6,14 +6,109 @@ This
 ## Data Stack
 
 - Nifi for ingestion
+- Nifi registry for control version
 - Minio for object storage (S3 AWS)
-
-- Spark convert to iceberg table
-- DBT for transformation, basic tests and documentation
 - Dremio as query engine
+- Apache Nessie for metastore
+
+
+- DBT for transformation, basic tests and documentation
+
 - Jupyter notebook for data quality
 - Airflow orchestration
-- Nessie for metastore
+
+# Environment setup
+
+## Install docker
+Download and install the docker desktop.
+
+## Python install (optional)
+
+### create a venv environment
+```sh
+py -m venv venv
+````
+
+### install the requirements
+```sh
+pip install -r requirements.txt
+```
+
+# Pipeline
+
+## From a local folder to S3
+
+Execute the bellow command to launch Nifi.
+```sh
+docker compose --profile nifi up -d
+````
+- https://localhost:8443/nifi/#/login
+- user: nifi
+- password: nifi12345678
+
+
+On Nifi add a new Process group and import the json file according the following the steps below.
+
+1. Drag and drop the Process group icon
+
+<img src="docs/img/nifi-1.png">
+
+2. Click on the icon browser and import the json file presented on the path "nifi/Ingestion_raw_data.json"
+
+<img src="docs/img/nifi-2.png">
+
+<img src="docs/img/nifi-3.png">
+
+3. Open the group Ingestion_raw_data, after open the file_ingestion_to_s3 group process.
+
+
+<img src="docs/img/nifi-4.png">
+
+4. Open the process PutS3Object, go to the properties tab, on AWS Credentials Provider Services, select the option 'Go to Service'
+
+<img src="docs/img/nifi-5.png">
+<img src="docs/img/nifi-6.png">
+
+5. Add the Access and Secret Keys. Apply the changes.
+
+<img src="docs/img/nifi-7.png">
+<img src="docs/img/nifi-8.png">
+
+6. Enable the service using the scope 'Service and referencing components'. 
+
+<img src="docs/img/nifi-9.png">
+<img src="docs/img/nifi-10.png">
+
+7. Close returning to the group process. The PutS3Object now is ready to send files to s3.
+
+8. Return to the first level and start the Ingestion_raw_data process group.
+
+<img src="docs/img/nifi-11.png">
+
+9. Paste the csv file on the folder datasources inside the repository. It will be automatically sent to Minio on under the bucket landing-zone and delete the file.
+
+<img src="docs/img/nifi-12.png">
+
+Notes:
+- this approach took in consideration only the file sent for the assessment. For a different file is needed an adjustment in the flow.
+
+## From a Landing Zone on S3 to a Bronze layer
+
+Execute the bellow command to launch Dremio.
+```sh
+docker compose --profile dremio up -d
+````
+- http://localhost:9047/
+
+1. Access Dremio UI and set the admin properties as per the images below.
+
+<img src="docs/img/dremio-1.png">
+<img src="docs/img/dremio-2.png">
+
+2. On the project repository, execute the Python script "scripts/dremio_add_sources.py" to add the sources through Dremio API.
+
+<img src="docs/img/dremio-3.png">
+
 
 ## Tools and credentials
 
@@ -22,4 +117,8 @@ https://localhost:8443/nifi/#/login
 user: nifi
 password: nifi12345678
 
-#
+### Minio IO
+http://localhost:9001/
+user:minio
+password: minio123
+
